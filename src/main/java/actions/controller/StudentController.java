3eb30941eb4ff2ed63f99dcad621d907684763a0
Jeonikjoo <zza1541@gmail.com>
2024-12-27 15:30:55 +0900

    update project

--- src/main/java/actions/controller/StudentController.java
+++ src/main/java/actions/controller/StudentController.java
@@ -1,8 +1,16 @@
 package actions.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import actions.service.StudentService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class StudentController {
+	private final StudentService studentService;
+	
 	
 }
--- src/main/java/actions/repository/StudentRepository.java
+++ src/main/java/actions/repository/StudentRepository.java
@@ -7,5 +7,5 @@ import actions.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+	
 }
